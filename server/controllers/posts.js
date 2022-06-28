40d3ffb8a88f2c0324a6ad2620909198e4ce6ca2
Dastaan <vaibhav_b200642ce@nitc.ac.in>
2022-06-29 01:12:40 +0530

    added google login logout

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,6 @@
 import PostMessage from '../models/postMessage.js';
 import express from 'express';
+import mongoose from 'mongoose';
 
 export const getPosts = async (req,res) => {
     try{
@@ -25,3 +26,34 @@ export const createPost = async (req,res) => {
     }
 
     }
+
+export const updatePost = async(req,res) => {
+    const {id: _id} = req.params;
+    const post = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id')
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id},{ new: true});
+    res.json(updatedPost);
+}
+
+export const deletePost = async(req,res) => {
+    const {id:_id}  = req.params;
+    const post = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+    await PostMessage.findByIdAndRemove(_id);
+    res.json({message: 'Post deleted'});
+}
+
+export const likePost = async(req,res) =>{
+    const {id} = req.params;
+    
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, {likeCount: post.likeCount + 1}, {new:true})
+
+    res.json(updatedPost);
+}
--- server/index.js
+++ server/index.js
@@ -2,21 +2,20 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
-
+import dotenv from 'dotenv'
 import postRoutes from "./routes/posts.js";
 
+dotenv.config();
 const app = express();
-
 
 app.use(bodyParser.json({ limit: "30mb" , extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL =  'mongodb://dastaan:prachi123@ac-71nql7o-shard-00-00.stmcmf0.mongodb.net:27017,ac-71nql7o-shard-00-01.stmcmf0.mongodb.net:27017,ac-71nql7o-shard-00-02.stmcmf0.mongodb.net:27017/?ssl=true&replicaSet=atlas-wiucup-shard-0&authSource=admin&retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL).then(() => {
+mongoose.connect(process.env.CONNECTION_URL).then(() => {
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     })
